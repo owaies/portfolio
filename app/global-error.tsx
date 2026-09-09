b9b41dd82edde3cc45996ac1bd6e54cbaf0aa148
owaies <owaies786@gmail.com>
2026-09-10 00:12:52 +0530

    reliability: preserve global error page styling

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect } from 'react'
+import './globals.css'
 
 export default function GlobalError({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
   useEffect(() => {
